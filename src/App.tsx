009bc463c5475280133d3b16d224e551be95496f
yuanda@laptop <34027873+yuandahanif@users.noreply.github.com>
2023-04-07 19:31:56 +0700

    feat: :sparkles: add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,11 @@ import { useState } from "react";
 function App() {
   const [count, setCount] = useState(0);
 
-  return <div className="App">moo-forum</div>;
+  return (
+    <div className="bg-red-200">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
 }
 
 export default App;
